2aed8fa595f3e96f4cf63a741b8355d403b6cb25
Montassar Silini <slnmonta2k17@gmail.com>
2020-08-12 13:18:45 +0200

    1st Version Done(Desktop irresponsive)

---
+++ Js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop()) {
+            $('nav').addClass('scrolledNav');
+        } else {
+            $('nav').removeClass('scrolledNav');
+        }
+    });
+
+    var scrollLink = $('.scroll');
+    var topPage = $('.navbar-brand');
+
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top - 74
+        }, 1000);
+        console.log($(this.hash));
+    });
+
+    topPage.click(function(e) {
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+        console.log($(this.hash));
+    });
+});
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 500,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
